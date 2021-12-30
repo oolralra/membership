f5732510317215c59541f730c825517531d8ecbf
oolralra <oolralra@gmail.com>
2021-12-30 10:28:59 +0900

    Delete membership directory

--- membership/common/dbconnect.php
+++
@@ -1,4 +0,0 @@
-<?php
-   $con=mysqli_connect("localhost","root","my1234%^&*","keduitdb")or die("mysql 접속 실패");
-  //echo 'db 접속 성공<br/>';
-?>
--- membership/common/sessionstart.php
+++
@@ -1,4 +0,0 @@
-<?php
-  session_start();  //세션을 시작
- // echo '세션 시작<br/>';
-?>
